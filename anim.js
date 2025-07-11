b42a2409fee566bc7154f016892f10d9fef12395
rkoorson <153790339+rkoorson@users.noreply.github.com>
2025-07-11 01:35:03 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -12,5 +12,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Opcional: Mensaje para el usuario (puedes personalizarlo)
   const titulo = document.querySelector(".titulo");
-  titulo.innerHTML += "<br><small>play</small>";
+  titulo.innerHTML += "<br><small>"play"</small>";
 });
